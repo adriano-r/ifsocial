73f1e606916833d9fa88f1d841ce0090c662c7d6
adriano-r <adrianorosa9974@gmail.com>
2021-11-30 22:21:02 -0300

    using dynamic data

---
+++ src/components/post/Post.jsx
@@ -0,0 +1,37 @@
+import "./post.css"
+import {MoreVert} from '@material-ui/icons'
+import {Users} from "../../dummyData";
+
+
+export default function Post({post}) {
+    return (
+        <div className="post">
+            <div className="postWrapper">
+                <div className="postTop">
+                    <div className="postTopLeft">
+                        <img src="/assets/person/1.jpeg" alt="" className="postProfileImg" />
+                        <span className="postUsername">{Users.filter((u) => u.id === post.userId)[0].username}</span>
+                        <span className="postDate">{post.date}</span>
+                    </div>
+                    <div className="postTopRight">
+                        <MoreVert />
+                    </div>
+                </div>
+                <div className="postCenter">
+                    <span className="postText">{post?.desc}</span>
+                    <img className="postImg" src={post.photo} alt="" />
+                </div>
+                <div className="postBottom">
+                    <div className="postBottomLeft">
+                        <img src="/assets/like.png" alt="" className="likeIcon" />
+                        <img src="/assets/heart.png" alt="" className="likeIcon" />
+                        <span className="postLikeCounter">{post.like} liked it</span>
+                    </div>
+                    <div className="postBottomRight">
+                        <span className="postCommentText">{post.comment} comments</span>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
